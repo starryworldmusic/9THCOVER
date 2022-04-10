ecea718b022381054041921a52e1e293136adfef
starryworldmusic <103361264+starryworldmusic@users.noreply.github.com>
2022-04-10 19:02:30 +0800

    Update app.py

--- website/app.py
+++ website/app.py
@@ -53,7 +53,7 @@ with st.container():
         st.image(img_cover2)
     st.write("感謝大家一起參與「9th Must Go On cover歌錄音大賽」的評審工作，每次你會聽到兩首參賽作品，請投選你認為表現更佳的作品，每天最多可以投票10次。")
     st.write("勝出者可以獲豐富獎品包括：Planet Beyond時尚藍牙耳機(價值：$1580）及獎狀乙張")
-    st.write("投票觀眾亦有機會獲得星格學卷$100，共會抽5名幸運員 ")
+    st.write("投票觀眾亦有機會獲得星格學卷$100，共會抽5名幸運兒 ")
     
 #date process  
 time = datetime.datetime.now()
