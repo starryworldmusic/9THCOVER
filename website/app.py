5e7894f93a4474c51e8e678dd5f9b632866d8ca8
starryworldmusic <103361264+starryworldmusic@users.noreply.github.com>
2022-04-11 19:51:57 +0800

    Update app.py

--- website/app.py
+++ website/app.py
@@ -13,6 +13,29 @@ import random
 import datetime
 from PIL import Image
 from streamlit_player import st_player
+from supabase import create_client, Client
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    return create_client(url, key)
+
+supabase = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query():
+    return supabase.table("data").select("*").execute()
+
+rows = run_query()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['song_name']}:")
 
 #intial session state
 if 'admited' not in st.session_state:
