0e65f922666cecc51646385fc640f21f249bf506
starryworldmusic <103361264+starryworldmusic@users.noreply.github.com>
2022-04-12 20:47:26 +0800

    Update app.py

--- website/app.py
+++ website/app.py
@@ -30,6 +30,10 @@ if 'admited' not in st.session_state:
     st.session_state['admited'] = 0
 if 'repeatvote' not in st.session_state:
     st.session_state['repeatvote'] = 0
+if 'nowvote' not in st.session_state:
+    st.session_state['nowvote'] = 0
+if 'newcount' not in st.session_state:
+    st.session_state['newcount'] = 0
 if 'votesong' not in st.session_state:
     st.session_state['votesong'] = ""
 if 'song1' not in st.session_state:
@@ -37,9 +41,9 @@ if 'song1' not in st.session_state:
 if 'song2' not in st.session_state:
     st.session_state['song2'] = ""
 if 'r1' not in st.session_state:
-    st.session_state['r1'] = random.randint(0,9)
+    st.session_state['r1'] = random.randint(1,10)
 if 'r2' not in st.session_state:
-    st.session_state['r2'] = random.randint(0,9)
+    st.session_state['r2'] = random.randint(1,10)
 if 'counter' not in st.session_state:
     st.session_state['counter'] = 0
 if 'remain' not in st.session_state:
@@ -47,7 +51,7 @@ if 'remain' not in st.session_state:
 if 'number' not in st.session_state:
     st.session_state['number'] = 88888888
 while st.session_state['r1'] == st.session_state['r2']:
-    st.session_state['r1'] = random.randint(0,9)
+    st.session_state['r1'] = random.randint(1,10)
 
 #top bar
 
@@ -64,14 +68,52 @@ with st.container():
         st.image(img_cover2)
     st.write("感謝大家一起參與「9th Must Go On cover歌錄音大賽」的評審工作，每次你會聽到兩首參賽作品，請投選你認為表現更佳的作品，每天最多可以投票10次。")
     st.write("勝出者可以獲豐富獎品包括：Planet Beyond時尚藍牙耳機(價值：$1580）及獎狀乙張")
-    st.write("投票觀眾亦有機會獲得星格學卷$100，共會抽5名幸運員 ")
+    st.write("投票觀眾亦有機會獲得星格學卷$100，共會抽5名幸運兒 ")
     
 #date process  
+datasql = supabase.table("data").select("*").execute()
+numbersql = supabase.table("number").select("*").execute()
+df = pd.DataFrame(datasql.data).set_index("id")
+number_df = pd.DataFrame(data = numbersql.data).set_index("number")
 time = datetime.datetime.now()
 today = str(time.strftime("%Y"+"-"+"%m"+"-"+"%d"))
-number_df = pd.read_csv('website/number.csv', sep=',',index_col="number")
-df = pd.read_csv('website/data.csv', sep=',')
 
+def updatevote1():
+    datasql = supabase.table("data").select("*").execute()
+    df = pd.DataFrame(datasql.data).set_index("id")
+    st.session_state['nowvote'] = df.loc[st.session_state['r1'],"vote"]
+    st.session_state['nowvote'] += 1
+    new1 = int(st.session_state['nowvote'])
+    rr1 = int(st.session_state['r1'])
+    total1 = int(df.loc[st.session_state['r1'],"total"])
+    total1 +=1
+    supabase.table("data").update({"total":total1}).eq("id",rr1).execute()
+    supabase.table("data").update({"vote":new1}).eq("id",rr1).execute()
+    
+def updatevote2():
+    datasql = supabase.table("data").select("*").execute()
+    df = pd.DataFrame(datasql.data).set_index("id")
+    st.session_state['nowvote'] = df.loc[st.session_state['r2'],"vote"]
+    st.session_state['nowvote'] += 1
+    new2 = int(st.session_state['nowvote'])
+    rr2 = int(st.session_state['r2'])
+    total2 = int(df.loc[st.session_state['r2'],"total"])
+    total2 +=1
+    supabase.table("data").update({"total":total2}).eq("id",rr2).execute()
+    supabase.table("data").update({"vote":new2}).eq("id",rr2).execute()
+    
+def updatecount():
+    numbersql = supabase.table("number").select("*").execute()
+    number_df = pd.DataFrame(data = numbersql.data).set_index("number")
+    st.session_state['newcount'] = number_df.loc[st.session_state['number'],today]
+    st.session_state['newcount'] += 1
+    count1 = int(st.session_state['newcount'])
+    supabase.table("number").update({today:count1}).eq("number",st.session_state['number']).execute()
+    numbersql = supabase.table("number").select("*").execute()
+    number_df = pd.DataFrame(data = numbersql.data).set_index("number")
+    st.session_state['counter'] = int(number_df.loc[st.session_state['number'],today])
+    st.session_state['remain']  = 10 - st.session_state['counter']
+    
 def voting():
     if st.session_state['counter'] >= 10:
         st.error(str(st.session_state['number'])+"的使用者你好，今日你已經完成了所有投票，明天可以繼續。") 
@@ -80,14 +122,14 @@ def voting():
         st.info(str(st.session_state['number'])+"的使用者你好，今日你已經投了"+ str(st.session_state['counter']) +"次投票，你今天最多可多投"+str(st.session_state['remain'])+"次投票")                 
         left_column, right_column = st.columns(2)
         with left_column:
-            st.header("Song1 - "+df.iloc[st.session_state['r1'],4])
-            st_player(df.iloc[st.session_state['r1'],1])
+            st.header("Song1 - "+df.loc[st.session_state['r1'],"song_name"])
+            st_player(df.loc[st.session_state['r1'],"song"])
             
         with right_column:
-            st.header("Song2 - "+df.iloc[st.session_state['r2'],4])
-            st_player(df.iloc[st.session_state['r2'],1])
-        st.session_state['song1'] = str("Song1 - "+df.iloc[st.session_state['r1'],4])
-        st.session_state['song2'] = str("Song2 - "+df.iloc[st.session_state['r2'],4])
+            st.header("Song2 - "+df.loc[st.session_state['r2'],"song_name"])
+            st_player(df.loc[st.session_state['r2'],"song"])
+        st.session_state['song1'] = str("Song1 - "+df.loc[st.session_state['r1'],"song_name"])
+        st.session_state['song2'] = str("Song2 - "+df.loc[st.session_state['r2'],"song_name"])
         song = [st.session_state['song1'],st.session_state['song2']]
         st.session_state['votesong'] = st.radio('請投選你認為表現更佳的作品',song,key='vote_radio')    
         st.button("投票",on_click=voted)
@@ -96,27 +138,18 @@ def voting():
 def voted():
     st.session_state['repeatvote'] = 1
     if st.session_state['vote_radio'] == st.session_state['song1']:
-        df.iloc[st.session_state['r1'],5] += 1
-        df.to_csv("website/data.csv", index=False)
-        number_df.loc[st.session_state['number'],today] += 1
-        number_df.to_csv("website/number.csv")
-        st.session_state['counter'] = int(number_df.loc[st.session_state['number'],today])
-        st.session_state['remain']  = 10 - st.session_state['counter']
+        updatevote1()
+        updatecount()
     elif st.session_state['vote_radio'] == st.session_state['song2']:
-        df.iloc[st.session_state['r2'],5] += 1
-        df.to_csv("website/data.csv", index=False)
-        st.session_state['check_submit'] = 0
-        number_df.loc[st.session_state['number'],today] += 1
-        number_df.to_csv("website/number.csv")
-        st.session_state['counter'] = int(number_df.loc[st.session_state['number'],today])
-        st.session_state['remain']  = 10 - st.session_state['counter']
+        updatevote2()
+        updatecount()
 
 def refesh():
     st.session_state['repeatvote'] = 0
-    st.session_state['r1'] = random.randint(0,9)
-    st.session_state['r2'] = random.randint(0,9)
+    st.session_state['r1'] = random.randint(1,10)
+    st.session_state['r2'] = random.randint(1,10)
     while st.session_state['r1'] == st.session_state['r2']:
-        st.session_state['r1'] = random.randint(0,9)
+        st.session_state['r1'] = random.randint(1,10)
     
 with st.container():
     with st.form(key="checknumber"):
@@ -144,29 +177,11 @@ if st.session_state['admited'] == 1:
             st.success('你成功投左'+st.session_state['votesong']+'一票，請按下方更新歌曲button')
             left_column, right_column = st.columns(2)
             with left_column:
-                st.write("剛才song1 - "+df.iloc[st.session_state['r1'],4]+"的演唱歌手是"+df.iloc[st.session_state['r1'],3]+'，以下是他的cover封面設計。')
-                img1 = Image.open("website/images/"+df.iloc[st.session_state['r1'],2])
+                st.write("剛才song1 - "+df.loc[st.session_state['r1'],"song_name"]+"的演唱歌手是"+df.loc[st.session_state['r1'],"name"]+'，以下是他的cover封面設計。')
+                img1 = Image.open("website/images/"+df.loc[st.session_state['r1'],"photo"])
                 st.image(img1)
             with right_column:
-                st.write("剛才song2 - "+df.iloc[st.session_state['r2'],4]+"的演唱歌手是"+df.iloc[st.session_state['r2'],3]+'，以下是他的cover封面設計。')
-                img2 = Image.open("website/images/"+df.iloc[st.session_state['r2'],2])
+                st.write("剛才song2 - "+df.loc[st.session_state['r2'],"song_name"]+"的演唱歌手是"+df.loc[st.session_state['r2'],"name"]+'，以下是他的cover封面設計。')
+                img2 = Image.open("website/images/"+df.loc[st.session_state['r2'],"photo"])
                 st.image(img2)
-            st.button(label="更新下一組歌曲",on_click=refesh)
-                
-# Perform query.
-def run_query():
-    return supabase.table("data").select("*").execute()
-def run_query2():
-    return supabase.table("number").select("2022-04-11").execute()
-
-supabase.table("number").update({"2022-04-11":10}).eq("number",65774755).excute()
-
-data = run_query()
-number = run_query2()
-
-st.write(number)
-st.write(number.data)
-
-# Print results.
-for row in data.data:
-    st.write(row['name'])
+            st.button(label="更新下一組歌曲",on_click=refesh)         
